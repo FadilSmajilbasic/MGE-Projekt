168b4c7d3dd527fe6bef358718a1e920334cdb75
Niklas <n.kaesler@icloud.com>
2021-10-09 11:22:58 +0200

    added division/multiplication and fixed minus decimals

--- app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/DivisionActivity.java
+++ app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/DivisionActivity.java
@@ -3,12 +3,45 @@ package com.smajilbasic.kaesler.mgeprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DivisionActivity extends AppCompatActivity {
+public class DivisionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_division);
+        findViewById(R.id.calculateButton).setOnClickListener(this);
+        resultBox = findViewById(R.id.result);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.calculateButton) {
+
+            String firstInput = ((EditText) findViewById(R.id.firstNumber)).getText().toString();
+            String secondInput = ((EditText) findViewById(R.id.secondNumber)).getText().toString();
+
+            if (!firstInput.isEmpty()) {
+                if (!secondInput.isEmpty()) {
+                    StringBuilder builder = new StringBuilder();
+                    Double firstNumber = Double.valueOf(firstInput);
+                    Double secondNumber = Double.valueOf(secondInput);
+                    builder.append(firstInput).append(" / ").append(secondInput).append(" = ").append(firstNumber / secondNumber).append("\n");
+                    resultBox.append(builder.toString());
+                } else {
+                    Toast.makeText(this, getString(R.string.second_number_error_message_de), Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.first_number_error_message_de), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
