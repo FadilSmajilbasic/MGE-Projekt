692323488718e90ca5535391c8603f1d9108d589
Fadil Smajilbasic <smajilbaicfadil@gmail.com>
2021-10-09 13:29:36 +0200

    Added Dark mode setting in SettingsActivity

--- app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/SettingsActivity.java
+++ app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/SettingsActivity.java
@@ -1,14 +1,46 @@
 package com.smajilbasic.kaesler.mgeprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SwitchCompat themeSwitch = (SwitchCompat) findViewById(R.id.theme_switch);
+
+        int nightModeFlags =
+                themeSwitch.getContext().getResources().getConfiguration().uiMode &
+                        Configuration.UI_MODE_NIGHT_MASK;
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                themeSwitch.setChecked(true);
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                themeSwitch.setChecked(false);
+                break;
+        }
+        themeSwitch.setOnCheckedChangeListener(this);
+
+    }
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if(isChecked){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
