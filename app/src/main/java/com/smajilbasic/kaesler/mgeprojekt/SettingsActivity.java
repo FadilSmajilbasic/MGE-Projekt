328155971e8b56213183ab2efd73b2f59a609490
Fadil Smajilbasic <smajilbaicfadil@gmail.com>
2021-10-09 22:01:22 +0200

    Added dark mode switch, language spinner and Shared preference settings

--- app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/SettingsActivity.java
+++ app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/SettingsActivity.java
@@ -1,46 +1,108 @@
 package com.smajilbasic.kaesler.mgeprojekt;
 
+import static com.smajilbasic.kaesler.mgeprojekt.Helper.DARK_MODE_KEY;
+import static com.smajilbasic.kaesler.mgeprojekt.Helper.LOCALE_VALUE_KEY;
+import static com.smajilbasic.kaesler.mgeprojekt.Helper.updateLocale;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.widget.Spinner;
 
-public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+import com.google.android.material.textview.MaterialTextView;
+
+public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, AdapterView.OnItemSelectedListener {
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        sharedPref = getPreferences(MODE_PRIVATE);
+        editor = sharedPref.edit();
+        boolean darkModeSetting = sharedPref.getBoolean(DARK_MODE_KEY, false);
 
-        SwitchCompat themeSwitch = (SwitchCompat) findViewById(R.id.theme_switch);
-
-        int nightModeFlags =
-                themeSwitch.getContext().getResources().getConfiguration().uiMode &
-                        Configuration.UI_MODE_NIGHT_MASK;
-        switch (nightModeFlags) {
-            case Configuration.UI_MODE_NIGHT_YES:
-                themeSwitch.setChecked(true);
-                break;
-            case Configuration.UI_MODE_NIGHT_NO:
-            case Configuration.UI_MODE_NIGHT_UNDEFINED:
-                themeSwitch.setChecked(false);
-                break;
-        }
+        SwitchCompat themeSwitch = findViewById(R.id.theme_switch);
         themeSwitch.setOnCheckedChangeListener(this);
+        themeSwitch.setChecked(darkModeSetting);
 
+        initSpinner();
     }
 
+    @Override
+    public void applyOverrideConfiguration(Configuration overrideConfiguration) {
+        String lang = getPreferences(MODE_PRIVATE).getString(Helper.LOCALE_VALUE_KEY, "de");
+
+        super.applyOverrideConfiguration(updateLocale(this, lang).getResources().getConfiguration());
+    }
+
+    private void initSpinner() {
+        Spinner spinner = findViewById(R.id.language_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.languages_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        if (sharedPref.contains(LOCALE_VALUE_KEY)) {
+            String currentLocale = sharedPref.getString(LOCALE_VALUE_KEY, "de");
+            if (currentLocale.equals("en")) {
+                spinner.setSelection(adapter.getPosition("English"));
+            } else if (currentLocale.equals("it")) {
+                spinner.setSelection(adapter.getPosition("Italian"));
+            } else {
+                spinner.setSelection(adapter.getPosition("German"));
+            }
+        }
+
+        spinner.setOnItemSelectedListener(this);
+    }
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-        if(isChecked){
+        if (isChecked) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        }else{
+        } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String lang;
+        if(view !=null) {
+            String selected = view.toString();
+
+            switch (selected) {
+                default:
+                case "English":
+                    lang = "en";
+                    break;
+                case "Italian":
+                    lang = "it";
+                    break;
+                case "German":
+                    lang = "de";
+                    break;
+            }
+
+            editor.putString(LOCALE_VALUE_KEY, lang);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
