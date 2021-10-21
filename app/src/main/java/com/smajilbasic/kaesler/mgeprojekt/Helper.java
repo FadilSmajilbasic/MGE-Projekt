7e4b2d1dafc179dfb23c63e6f3b8e2ad8f06bd7f
Fadil Smajilbasic <smajilbaicfadil@gmail.com>
2021-10-21 16:32:33 +0200

    Added static method to retrieve theme id

--- app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/Helper.java
+++ app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/Helper.java
@@ -1,8 +1,11 @@
 package com.smajilbasic.kaesler.mgeprojekt;
 
+import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.util.Log;
 
 import java.util.Locale;
 
@@ -13,5 +16,10 @@ public class Helper {
     protected static final String USER_PREFERENCES = "user_preferences_name";
     protected static final String DARK_MODE_KEY = "dark_mode_setting";
     protected static final String THEME_KEY = "theme_to_set";
+
+    public static int getThemeId(Application application, SharedPreferences sharedPref){
+        String themeName = sharedPref.getString(THEME_KEY,"Theme.MGEProjekt");
+        return application.getResources().getIdentifier(themeName,"style", application.getPackageName());
+    }
 
 }
