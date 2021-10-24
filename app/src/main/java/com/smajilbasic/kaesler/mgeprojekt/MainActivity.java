34895992c1851ad79de5fdbf85b8b5da235ea068
Fadil Smajilbasic <smajilbaicfadil@gmail.com>
2021-10-24 18:20:43 +0200

    Added Greeting Notification

--- app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/MainActivity.java
+++ app/src/main/java/com/smajilbasic/kaesler/mgeprojekt/MainActivity.java
@@ -11,19 +11,35 @@ package com.smajilbasic.kaesler.mgeprojekt;
 
 import static com.smajilbasic.kaesler.mgeprojekt.Helper.DARK_MODE_KEY;
 import static com.smajilbasic.kaesler.mgeprojekt.Helper.HISTORY_FILENAME;
+import static com.smajilbasic.kaesler.mgeprojekt.Helper.NOTIFICATION_KEY;
 import static com.smajilbasic.kaesler.mgeprojekt.Helper.USER_PREFERENCES;
 import static com.smajilbasic.kaesler.mgeprojekt.Helper.getThemeId;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String CHANNEL_ID = "MGE.APP.CHANNEL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,20 +68,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (sharedPref.getBoolean(DARK_MODE_KEY, false)) {
                 if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    plusButton.setImageResource(R.drawable.plus_light);
-                    minusButton.setImageResource(R.drawable.minus_light);
-                    divideButton.setImageResource(R.drawable.multiply_light);
-                    multiplyButton.setImageResource(R.drawable.divide_light);
-                    historyButton.setImageResource(R.drawable.history_light);
-                }
-            } else {
-                if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                     plusButton.setImageResource(R.drawable.minus);
                     minusButton.setImageResource(R.drawable.plus);
                     divideButton.setImageResource(R.drawable.divide);
                     multiplyButton.setImageResource(R.drawable.multiply);
                     historyButton.setImageResource(R.drawable.history);
+                }
+            } else {
+                if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    plusButton.setImageResource(R.drawable.plus_light);
+                    minusButton.setImageResource(R.drawable.minus_light);
+                    divideButton.setImageResource(R.drawable.multiply_light);
+                    multiplyButton.setImageResource(R.drawable.divide_light);
+                    historyButton.setImageResource(R.drawable.history_light);
                 }
             }
         } else {
@@ -74,8 +90,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
 
+        if(sharedPref.getBoolean(NOTIFICATION_KEY,true))
+            greetingNotification();
+    }
+
+    private void greetingNotification() {
+        createNotificationChannel();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.calculator_icon)
+                .setContentTitle(getString(R.string.Notification_title_de))
+                .setContentText(getString(R.string.greeting_notification_text_de) + dateFormat.format(date))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
 
 
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        int notificationId = 123456;
+        notificationManager.notify(notificationId, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
